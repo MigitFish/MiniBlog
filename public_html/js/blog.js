b24af0981771fd72269563b86368864a6f81a1e7
Cannon Nielsen <can_man_14@yahoo.com>
2016-04-05 08:47:13 -0700

    MiniBlog B9 In this video i added a user to backendless through java scrpit

--- public_html/js/blog.js
+++ public_html/js/blog.js
@@ -1,7 +1,13 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+$(function (){
+    var APPLICATION_ID = "7B3ED48F-7D48-97AC-FFF1-5C3668EC8800",
+        SECRET_KEY = "5A4ACFAD-0153-90D1-FF3A-EF8D441A4300",
+        VERSION = "v1";
+        
+        Backendless.initApp(APPLICATION_ID, SECRET_KEY, VERSION);
+        
+        var user = new Backendless.User();
+        user.email = "nielsencannon14@yahoo.com",
+        user.passwrod = "password";
+        Backendless.UserService.register(user);
+})
 
